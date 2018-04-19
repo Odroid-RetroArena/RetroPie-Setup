062e9e6217c8d9efe0ae875c5c63af01086589a2
sikotik <33843126+sikotik@users.noreply.github.com>
2018-04-19 12:35:25 -0700

    Update xpad.sh

--- scriptmodules/supplementary/xpad.sh
+++ scriptmodules/supplementary/xpad.sh
@@ -14,12 +14,12 @@ rp_module_desc="Updated Xpad Linux Kernel driver"
 rp_module_help="This is the latest Xpad driver from https://github.com/paroj/xpad\n\nThe driver has been patched to allow the triggers to map to buttons for any controller and this has been enabled by default.\n\nThis fixes mapping the triggers in Emulation Station.\n\nIf you want the previous trigger behaviour please edit /etc/modprobe.d/xpad.conf and set triggers_to_buttons=0"
 rp_module_licence="GPL2 https://www.kernel.org/pub/linux/kernel/COPYING"
 rp_module_section="driver"
-rp_module_flags="noinstclean !mali"
+rp_module_flags="noinstclean "
 
 function depends_xpad() {
     local depends=(dkms)
     isPlatform "rpi" && depends+=(raspberrypi-kernel-headers)
-    isPlatform "x11" && depends+=(linux-headers-generic)
+    isPlatform "mali" && depends+=(linux-headers-generic)
     getDepends "${depends[@]}"
 }
 
