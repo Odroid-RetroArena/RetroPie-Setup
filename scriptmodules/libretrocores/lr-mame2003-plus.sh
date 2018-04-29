ce4f272f9dd0a7e9648a130069c1ff53d00a20a1
sikotik <33843126+sikotik@users.noreply.github.com>
2018-04-28 17:28:00 -0700

    Update lr-mame2003-plus.sh

--- scriptmodules/libretrocores/lr-mame2003-plus.sh
+++ scriptmodules/libretrocores/lr-mame2003-plus.sh
@@ -9,24 +9,24 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="lr-mame2003-plus"
+rp_module_id="mame2003_plus"
 rp_module_desc="Arcade emu - updated MAME 0.78 port for libretro with added game support"
 rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2003-plus-libretro/master/LICENSE.md"
 rp_module_section="exp"
 
-function sources_lr-mame2003-plus() {
+function sources_mame2003_plus() {
     gitPullOrClone "$md_build" https://github.com/libretro/mame2003-plus-libretro.git
 }
 
-function build_lr-mame2003-plus() {
+function build_mame2003_plus() {
     build_lr-mame2003
 }
 
-function install_lr-mame2003-plus() {
+function install_mame2003_plus() {
     install_lr-mame2003
 }
 
-function configure_lr-mame2003-plus() {
+function configure_mame2003_plus() {
     configure_lr-mame2003
 }
